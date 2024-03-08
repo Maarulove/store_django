90fa5e00c6425b6d052d023bb9b3492bf9b06167
Maarulove <qudayev62@mail.ru>
2024-03-08 11:09:45 +0100

    done smth

--- hometask/settings.py
+++ hometask/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-
 if os.environ.get("DEBUG") == "False":
     DEBUG = False
 else:
@@ -93,7 +92,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'hometask.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
